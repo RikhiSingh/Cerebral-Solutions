dfcdf4a3ab149236cfd854c1c0cebcaf8dbea020
Rikhi Singh <114336052+RikhiSingh@users.noreply.github.com>
2025-01-15 18:08:09 -0500

    fixed typo

--- app/(settings)/settings/layout.tsx
+++ app/(settings)/settings/layout.tsx
@@ -2,11 +2,11 @@ import { auth } from "@/auth";
 import { SessionProvider } from "next-auth/react";
 import SettingsNavbar from "./_components/settings-navbar";
 
-interface ProptectedLayoutProps {
+interface ProtectedLayoutProps {
     children: React.ReactNode
 }
 
-const ProtectedLayout = async ({ children }: ProptectedLayoutProps) => {
+const ProtectedLayout = async ({ children }: ProtectedLayoutProps) => {
     const session = await auth();
 
     return (
